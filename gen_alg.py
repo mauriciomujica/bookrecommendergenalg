8df0f8aed57a4f678763b0a69d6eb88e018d03af
Mauricio Mujica <mauricio.mujica@hotmail.com>
2024-12-21 14:51:42 -0300

    added similarityCalc

--- gen_alg.py
+++ gen_alg.py
@@ -2,6 +2,8 @@ import random
 from itertools import combinations
 import numpy as np
 import ast
+from statistics import mean
+from math import sqrt
 
 
 def initialPop(targetUser, ratings, books, M, N):
@@ -17,7 +19,7 @@ def initialPop(targetUser, ratings, books, M, N):
     return population
 
 
-def jaccard_similarity(v1, v2):
+def jaccardBooks(v1, v2):
     try:
         intersection = np.sum((v1 > 0) & (v2 > 0))
     except ValueError:
@@ -25,6 +27,66 @@ def jaccard_similarity(v1, v2):
         print(v2)
     union = np.sum((v1 > 0) | (v2 > 0))
     return intersection / (union + intersection) if union != 0 else 0
+
+
+def psim(targetuser, flat_users, ratings):
+    rated_targetuser = list(ratings[ratings["userID"] == targetuser]["ISBN"])
+    psim_value = 0
+
+    for user in flat_users:
+        same_books_rated = []
+        rated_user = list(ratings[ratings["userID"] == int(user)]["ISBN"])
+
+        for book in rated_user:
+            if book in rated_targetuser:
+                same_books_rated.append(book)
+
+        if len(same_books_rated) > 0:
+            for i in same_books_rated:
+                rating_tu = ratings[
+                    (ratings["ISBN"] == i) & (ratings["userID"] == targetuser)
+                ]["bookRating"].values[0]
+                rating_u = ratings[
+                    (ratings["ISBN"] == i) & (ratings["userID"] == user)
+                ]["bookRating"].values[0]
+                mean_tu = mean(
+                    list(ratings[ratings["userID"] == targetuser]["bookRating"])
+                )
+                mean_u = mean(list(ratings[ratings["userID"] == user]["bookRating"]))
+
+                numerator = (rating_tu - mean_tu) * (rating_u - mean_u)
+                denominator = sqrt((rating_tu - mean_tu) ** 2) * sqrt(
+                    (rating_u - mean_u) ** 2
+                )
+                if denominator != 0:
+                    value = numerator / denominator
+                    psim_value += value
+                else:
+                    psim_value += 0
+        else:
+            psim_value += 0
+    return psim_value
+
+
+def jaccardUsers(targetuser, flat_users, ratings):
+    rated_targetuser = list(ratings[ratings["userID"] == targetuser]["ISBN"])
+    jaccard_value = 0
+
+    for user in flat_users:
+        same_books_rated = []
+        rated_user = list(ratings[ratings["userID"] == int(user)]["ISBN"])
+
+        for book in rated_user:
+            if book in rated_targetuser:
+                same_books_rated.append(book)
+
+        if len(same_books_rated) > 0:
+            numerator = abs(len(same_books_rated))
+            denominator = abs(len(rated_targetuser) + len(rated_user))
+            value = numerator / denominator
+            jaccard_value += value
+
+    return jaccard_value
 
 
 def correlationCal(pop, books):
@@ -40,7 +102,7 @@ def correlationCal(pop, books):
                 raise ValueError(f"Item '{item}' not found in the dataframe.")
         correlations = []
         for i1, i2 in combinations(range(len(vectors)), 2):
-            corr = jaccard_similarity(vectors[i1], vectors[i2])
+            corr = jaccardBooks(vectors[i1], vectors[i2])
             correlations.append(corr)
 
         fitness_value = sum(correlations)
@@ -49,17 +111,38 @@ def correlationCal(pop, books):
     return fitness_scores
 
 
-def crossover(bestMem, r):
+def crossover(bestMem):
     newpop = []
-    c = 0
-    for i in bestMem:
+    for _ in range(len(bestMem)):
         pair = random.sample(bestMem, 2)
         i1, i2 = [i for i in pair]
         l1 = ast.literal_eval(i1)
         l2 = ast.literal_eval(i2)
-        while c <= r:
-            children = random.sample(l1, 3) + random.sample(l2, 3)
-            newpop.append(children)
-            c += 1
+        children = random.sample(l1, 3) + random.sample(l2, 3)
+        newpop.append(children)
 
     return newpop
+
+
+def similarityCal(ratings, newpop, user):
+    sim_scores = []
+    for individual in newpop:
+        total_users = []
+        for i in individual:
+            users = list(ratings[ratings["ISBN"] == i]["userID"])
+            if len(users) > 0:
+                total_users.append(users)
+        try:
+            flat_users = list(np.concatenate(total_users))
+        except ValueError:
+            print("individual that failed: ", individual)
+        psim_ind = psim(user, flat_users, ratings)
+        jac_ind = jaccardUsers(user, flat_users, ratings)
+        sim_of_ind = psim_ind * jac_ind
+        sim_scores.append(sim_of_ind)
+
+    return sim_scores
+
+
+# def crossover2():
+# another way of doing the crossover
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import gen_alg
-
+import ast
+import heapq
 
 if __name__ == "__main__":
     books = pd.read_csv("books_data/books.csv")
     ratings = pd.read_csv("books_data/ratings.csv")
-    user = 277157
-    M = 10
-    N = 6
-    R = 6
-    currentGen = 0
+    user = 277157  # userID
+    M = 1000  # initial size of pop
+    N = 10  # number of books inside of an individual
+    R = 0.8  # ratio of which the newpop is generated using the initialpop
+    currentGen = 5
     maxGen = 10
-    topX = 5
     pop = gen_alg.initialPop(user, ratings, books, M, N)
 
     while currentGen != maxGen:
@@ -20,9 +20,21 @@ if __name__ == "__main__":
         d_sorted = {
             k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)
         }
-        bestMem = list(d_sorted.keys())[0:topX]
-        newpop = gen_alg.crossover(bestMem, R)
-
+        bestMem = list(d_sorted.keys())[0 : round(len(d_sorted) * R)]
+        newpop = gen_alg.crossover(bestMem)
+        similarity = gen_alg.similarityCal(ratings, newpop, user)
+        d2 = {str(k): v for k, v in zip(newpop, similarity)}
+        d_sorted2 = {
+            k: v for k, v in sorted(d2.items(), key=lambda item: item[1], reverse=True)
+        }
+        bestMem2 = list(d_sorted2.keys())[0 : round(len(d_sorted2) * R)]
+        pop = bestMem2
         currentGen += 1
 
-print(len(newpop))
+print(d_sorted2)
+
+best = list(map(list, d_sorted2.items()))
+
+L = round(len(d_sorted) * R)
+
+test = list(heapq.nlargest(L, d_sorted, key=d_sorted.get))
